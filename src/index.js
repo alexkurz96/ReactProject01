f9a717c70ce48dd28bec8db70f3028ba71eae7b1
alexkurz96 <alex-kurz@mail.ru>
2019-12-03 22:19:43 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -3,29 +3,29 @@ import './main.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { createBrowserHistory } from 'history';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { browserHistory, Router, Route } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
+import { routerMiddleware, ConnectedRouter } from 'connected-react-router';
+//import { Router, Route } from 'react-router';
 import { Provider } from 'react-redux';
 
-import reducers from 'reducers';
+import createRootReducer from 'reducers';
 import Layout from 'containers/layout';
-import Phones from 'containers/phones';
+//import Phones from 'containers/phones';
 
-const store = createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunk)
+const history = createBrowserHistory();
+const middlewares = [thunk, routerMiddleware(history)];
+const store = createStore(createRootReducer(history), composeWithDevTools(
+	applyMiddleware(...middlewares)
 ));
 
-const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <Route component={Layout}>
-                <Route path='/' component={Phones} />
-            </Route>
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		<ConnectedRouter history={history}>
+			<Layout />
+		</ConnectedRouter>
+	</Provider>,
+	document.getElementById('root')
 );
